d2ec00cb048ebc502995ebcb5ace33d8dd1dafc9
suprabhath <81284730+suprabhath01401@users.noreply.github.com>
2023-01-14 04:29:57 +0530

    Update settings.py

--- grid_project/settings.py
+++ grid_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^bbml1cvi2%cs7m72n!k=$@8h1)$a4i6879h0v+k@83y1umy&z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ledproj.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
